650f6bec631590c16db28ca1c6a01619dfae662f
Hamza <hamzakhatri12345@gmail.com>
2024-06-26 10:02:21 +0500

    Initial commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import PostCard from '../components/PostCard';
 import NewPostModal from '../components/NewPostModal';
 import EditPostModal from '../components/EditPostModal';
@@ -27,24 +29,20 @@ export default class Home extends Component {
     }
 
     componentDidMount() {
-        // Fetch posts when the component mounts
         this.fetchPosts();
     }
 
-    // Fetch posts from the API
     fetchPosts = () => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                // Reverse the order of posts and update state
                 this.setState({ posts: response.data.reverse(), loading: false });
             })
             .catch(error => {
-                // Handle error if fetch fails
                 this.setState({ error: error.message, loading: false });
+                toast.error('Error fetching posts');
             });
     }
 
-    // Update a post via API PUT request
     updatePost = () => {
         const { editPost } = this.state;
         axios.put(`https://jsonplaceholder.typicode.com/posts/${editPost.id}`, editPost, {
@@ -53,43 +51,39 @@ export default class Home extends Component {
             }
         })
             .then(response => {
-                // Update posts state with the updated post
                 const updatedPosts = this.state.posts.map(post =>
                     post.id === editPost.id ? { ...post, title: editPost.title, body: editPost.body } : post
                 );
                 this.setState({ posts: updatedPosts, showEditModal: false, editPost: { id: null, title: '', body: '' } });
+                toast.success('Post updated successfully');
             })
             .catch(error => {
-                // Log error if update fails
                 console.error('There was an error updating the post!', error);
+                toast.error('Error updating post');
             });
     }
 
-    // Delete a post via API DELETE request
     deletePost = (id) => {
         axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(response => {
-                // Remove deleted post from posts state
                 const updatedPosts = this.state.posts.filter(post => post.id !== id);
                 this.setState({ posts: updatedPosts });
+                toast.success('Post deleted successfully');
             })
             .catch(error => {
-                // Log error if delete fails
                 console.error('Error deleting post:', error);
+                toast.error('Error deleting post');
             });
     }
 
-    // Open modal for creating a new post
     openModal = () => {
         this.setState({ showModal: true });
     }
 
-    // Close modal for creating a new post
     closeModal = () => {
         this.setState({ showModal: false });
     }
 
-    // Open modal for editing an existing post
     openEditModal = (post) => {
         this.setState({
             showEditModal: true,
@@ -101,12 +95,10 @@ export default class Home extends Component {
         });
     }
 
-    // Close modal for editing an existing post
     closeEditModal = () => {
         this.setState({ showEditModal: false, editPost: { id: null, title: '', body: '' } });
     }
 
-    // Handle input change for new post form
     handleInputChange = (e) => {
         const { name, value } = e.target;
         this.setState(prevState => ({
@@ -117,7 +109,6 @@ export default class Home extends Component {
         }));
     }
 
-    // Handle input change for edit post form
     handleEditInputChange = (e) => {
         const { name, value } = e.target;
         this.setState(prevState => ({
@@ -128,12 +119,10 @@ export default class Home extends Component {
         }));
     }
 
-    // Create a new post via API POST request
     createPost = () => {
         const { newPost } = this.state;
         axios.post('https://jsonplaceholder.typicode.com/posts', { ...newPost, id: Date.now() })
             .then(response => {
-                // Add newly created post to posts state
                 const updatedPosts = [response.data, ...this.state.posts];
                 this.setState({
                     posts: updatedPosts,
@@ -143,28 +132,28 @@ export default class Home extends Component {
                         body: ''
                     }
                 });
+                toast.success('Post created successfully');
             })
             .catch(error => {
-                // Log error if create fails
                 console.error('Error creating post:', error);
+                toast.error('Error creating post');
             });
     }
 
     render() {
         const { posts, loading, error, showModal, showEditModal, newPost, editPost } = this.state;
 
-        // Show loading message while fetching posts
         if (loading) {
             return <p className='text-center my-48'>Loading...</p>;
         }
 
-        // Show error message if fetch fails
         if (error) {
             return <p>Error: {error}</p>;
         }
 
         return (
             <div>
+                <ToastContainer />
                 <div className='bg-gray-200 py-16'>
                     <div className="container mx-auto px-6">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -187,14 +176,13 @@ export default class Home extends Component {
                             <PostCard
                                 key={post.id}
                                 post={post}
-                                handleEdit={this.openEditModal} // Pass openEditModal as handleEdit function
-                                handleDelete={this.deletePost} // Pass deletePost as handleDelete function
+                                handleEdit={this.openEditModal}
+                                handleDelete={this.deletePost}
                             />
                         ))}
                     </div>
                 </div>
 
-                {/* Modal for creating new post */}
                 {showModal && (
                     <NewPostModal
                         closeModal={this.closeModal}
@@ -205,7 +193,6 @@ export default class Home extends Component {
                     />
                 )}
 
-                {/* Modal for editing existing post */}
                 {showEditModal && (
                     <EditPostModal
                         closeModal={this.closeEditModal}
@@ -213,7 +200,6 @@ export default class Home extends Component {
                         updatePost={this.updatePost}
                         editPost={editPost}
                         loading={this.loading}
-
                     />
                 )}
             </div>
